a48b294400f9c0fbd32cd6a98e56075fc18495f3
stEugene <st_eugene@mail.ru>
2022-07-18 22:36:58 +0300

    исправления автотестов 9

--- controllers/users.js
+++ controllers/users.js
@@ -20,11 +20,11 @@ module.exports.findUser = (req, res) => {
   User.findById(req.params.id)
     .then((userFromBD) => {
       if (!userFromBD) {
-        return res.status(400).send({ message: `Произошла ошибка: Пользователь не найден`})
+        return res.status(404).send({ message: `Произошла ошибка: Пользователь не найден`})
       }
       res.send(userFromBD)
     })
-    .catch((err) => res.status(404).send({ message: `Произошла ошибка: ${err}`}));
+    .catch((err) => res.status(400).send({ message: `Произошла ошибка: ${err}`}));
 }
 
 module.exports.getAllUsers = (req, res) => {
@@ -32,14 +32,20 @@ module.exports.getAllUsers = (req, res) => {
     .then((dataFromDB) => res.send(dataFromDB))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err}`}))
 }
-
+//--------------------------------------------------------
 module.exports.profileUserUpdate = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {name: req.body.name, about: req.body.about},
     {new: true})
-      .then((dataFromDB) => res.send(`Новое имя пользователя ${dataFromDB.name}`))
-      .catch((err) => res.status(400).send({ message: `Произошла ошибка: ${err}`}));
+      .then((dataFromDB) => res.send({ message: `Новое имя пользователя ${dataFromDB.name}`}))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          return res.status(400).send({ message: `Произошла ошибка: ${err}`})
+        } else {
+          return res.status(500).send({ message: `Произошла ошибка: ${err}`})
+        }
+      })
 }
 
 module.exports.avatarUserUpdate = (req, res) => {
