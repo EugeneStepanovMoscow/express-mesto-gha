0be2545bc515a283bb1c88b7cc3f34b91bea8a1d
stEugene <st_eugene@mail.ru>
2022-07-24 23:00:13 +0300

    Изменение схемы User

--- models/user.js
+++ models/user.js
@@ -18,6 +18,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: mongoose.Schema.email,
+    required: true,
+    unique: true,
+    validate: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: true,
+  }
 });
 
 module.exports = mongoose.model('user', userSchema);
