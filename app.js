2463d59f5bbaffd4c92851e631e9336550ab45e8
stEugene <st_eugene@mail.ru>
2022-08-01 22:46:46 +0300

    Валидация запроса

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const routerUser = require('./routes/user');
 const routerCard = require('./routes/card');
 const { createUser, login, logout } = require('./controllers/users');
 const { authCheck } = require('./middlewares/auth');
-const { isOwner } = require('./middlewares/isOwner')
+const { isOwner } = require('./middlewares/isOwner');
+const { celebrate, Joi , errors} = require('celebrate');
 
 const { PORT = 3000 } = process.env; // присваиваем номер порта из окружения или 3000 по умолчанию
 
@@ -22,7 +23,15 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/signout', logout);
-app.use('/signup', createUser);
+app.use('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    // avatar:
+  }).unknown(true),
+}), createUser);
 app.use('/signin', login);
 
 app.use(authCheck);
@@ -34,8 +43,9 @@ app.use(isOwner);
 
 app.use('/cards', routerCard);
 
-
 app.use((req, res) => res.status(404).send({ message: 'Страница не найдена' }));
+
+app.use(errors()); // обработка ошибок сгенерированных Joi
 
 app.listen(PORT, () => {
   // console.log(`App listening on port ${PORT}`);
